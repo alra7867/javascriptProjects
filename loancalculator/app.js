28d263b6373e58dfa9a7e77405c36b3ac2d4485f
alra7867 <ali.raza-1@colorado.edu>
2019-09-10 11:12:37 -0600

    Final calculator

--- loancalculator/app.js
+++ loancalculator/app.js
@@ -1,8 +1,18 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+
+    document.getElementById('results').style.display = 'none';
+
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+
+});
 
 // Calculate Results
-function calculateResults(e){
+function calculateResults(){
   console.log('Calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
@@ -24,17 +34,26 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    document.getElementById('results').style.display = 'block';
+
+
+    document.getElementById('loading').style.display = 'none';
+
+
   } else {
       showError('Please check numbers');
   }
 
-  e.preventDefault();
 }
 
 
 
 function showError(error){
+    document.getElementById('results').style.display = 'none';
 
+
+    document.getElementById('loading').style.display = 'none';
+    
     const errorDiv = document.createElement('div');
 
     // get elements
